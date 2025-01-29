cbb503cdaf4ec348a2e63b02e4e48e300390edde
MaratK <yadeforz@gmail.com>
2025-01-30 00:09:50 +0300

    prometheus and grafana

--- UserService/Program.cs
+++ UserService/Program.cs
@@ -1,11 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 using EFCore.NamingConventions;
 using UserService.Data;
+using Prometheus;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddMetrics();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -22,11 +23,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 //app.UseAuthorization();
 
 app.MapControllers();
 
 app.MapGet("/health/", () => Results.Json(new { status = "OK" }));
+
+app.UseRouting();
+app.UseHttpMetrics(); // Add HTTP metrics
+app.MapMetrics(); // Map the Prometheus endpoint at /metrics
+app.MapControllers();
 
 app.Run();
